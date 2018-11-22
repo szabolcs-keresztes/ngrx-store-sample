51be445f780ebf4972d772fa1eee16866a7fe256
szabi.keresztes <szabi.keresztes@bjss.com>
2018-11-22 16:02:53 +0000

    Dispatching an action and handling that action successfully in the reducer function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,9 @@
 import { Component } from '@angular/core';
 
-import { Store } from '@ngrx/store';
-
-import { FaultType } from './data-models/fault-type';
-import { PickFaultType } from './store/actions';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
-  constructor(private store: Store<{ pickedFaultType: FaultType }>) {}
-
-  pickFaultType(faultType: FaultType) {
-    console.log('selected fault type', faultType);
-    this.store.dispatch(new PickFaultType(faultType));
-  }
-
 }
--- src/app/pages/test-report/test-report.component.ts
+++ src/app/pages/test-report/test-report.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+
+import { FaultType } from '../../data-models/fault-type';
+import { PickFaultType } from '../../store/actions';
 
 @Component({
   selector: 'app-test-report',
   templateUrl: './test-report.component.html',
   styleUrls: ['./test-report.component.css']
 })
-export class TestReportComponent implements OnInit {
+export class TestReportComponent {
 
-  constructor() { }
+  pickedFaultType$: Observable<FaultType>
 
-  ngOnInit() {
+  constructor(private store: Store<{ faults: { pickedFaultType: FaultType }}>) {}
+
+  pickDrivingFaultType() {
+    this.pickFaultType(FaultType.driving);
+  }
+
+  pickSeriousFaultType() {
+    this.pickFaultType(FaultType.serious);
+  }
+
+  pickDangerousFaultType() {
+    this.pickFaultType(FaultType.dangerous);
+  }
+
+  pickFaultType(faultType: FaultType) {
+    this.store.dispatch(new PickFaultType(faultType));
   }
 
 }
